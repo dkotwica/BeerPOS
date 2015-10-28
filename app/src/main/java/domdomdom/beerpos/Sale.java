50eb15f5c2445c943fec7157ab16d13c763c1888
dtp130 <dtp130@gmail.com>
2015-10-28 12:53:26 -0400

    added ammount functionality to the beerlist.  Each click will increase the ammount by 1.  This value is shown on the beerlist.

--- app/src/main/java/domdomdom/beerpos/Sale.java
+++ app/src/main/java/domdomdom/beerpos/Sale.java
@@ -24,6 +24,8 @@ public class Sale extends MainActivity {
     ArrayList<Integer> tabAmount;
     ArrayList<String> beerName;
     ArrayList<Double> beerValue;
+    ArrayList<Integer> beerClicks;
+
     ArrayList<String> beerItem = new ArrayList<String>();
     ArrayList<String> openTabs = new ArrayList<String>();
     String[] beerStepValues;
@@ -35,6 +37,7 @@ public class Sale extends MainActivity {
         tabAmount=new ArrayList<Integer>();
         beerName=new ArrayList<String>();
         beerValue=new ArrayList<>();
+        beerClicks = new ArrayList<>();
         final String[] beerStepValues = new String[32];
         for(int i = 0; i < beerStepValues.length; i++){
             String number = Double.toString((double)i/2 + 1);
@@ -52,6 +55,15 @@ public class Sale extends MainActivity {
         Button btEdit=(Button)findViewById(R.id.editBeer);
         //editText=(EditText)findViewById(R.id.txtInput);
         Button btAdd=(Button)findViewById(R.id.openTab);
+
+        listV2.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> a, View v, int position, long id) {
+                beerClicks.set(position, (beerClicks.get(position) + 1));
+                updateBeerList();
+            }
+        });
+
         btEdit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -79,6 +91,7 @@ public class Sale extends MainActivity {
                         double bv = np.getValue();
                         beerName.add(newBeer);
                         beerValue.add(bv/2 + 0.5);
+                        beerClicks.add(0);
                         beerItem.add("test");
                         updateBeerList();
                     }
@@ -146,7 +159,7 @@ public class Sale extends MainActivity {
     private void updateBeerList(){
 
         for(int i =0; i< beerName.size(); i++) {
-            beerItem.set(i, beerName.get(i) + "     " +"$" + beerValue.get(i));
+            beerItem.set(i, beerName.get(i) + "     " +"$" + beerValue.get(i) + "  Amt: "+ beerClicks.get(i));
         }
         adapter1.notifyDataSetChanged();
 
@@ -186,6 +199,7 @@ public class Sale extends MainActivity {
         alert.setNeutralButton("Add to Tab", new DialogInterface.OnClickListener() {
                     @Override
                 public void onClick(DialogInterface dialog, int which){
+
 
                     }
                 });
