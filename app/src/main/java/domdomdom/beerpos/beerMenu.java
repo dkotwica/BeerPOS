3ebc141b3493addb77cb9bc1d9440dc6fea0c0fb
dkotwica <domk123@gmail.com>
2015-11-29 13:21:22 -0500

    Beep

---
+++ app/src/main/java/domdomdom/beerpos/beerMenu.java
@@ -0,0 +1,13 @@
+package domdomdom.beerpos;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class beerMenu extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_beer_menu);
+    }
+}
