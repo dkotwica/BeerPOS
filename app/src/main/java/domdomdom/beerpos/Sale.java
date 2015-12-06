bf0b75af052dcb4d4a7f0bace9bf491ec956c0bc
dtp130 <dtp130@gmail.com>
2015-12-06 15:58:37 -0500

    History alert properly closes when dismissed.

--- app/src/main/java/domdomdom/beerpos/Sale.java
+++ app/src/main/java/domdomdom/beerpos/Sale.java
@@ -112,11 +112,13 @@ public class Sale extends MainActivity {
         listV.setOnItemClickListener(new AdapterView.OnItemClickListener() {
                                          @Override
                                          public void onItemClick(AdapterView<?> a, View v, int position, long id) {
+
                                              AlertDialog.Builder alertHistory = new AlertDialog.Builder(
                                                      Sale.this);
 
 
                                              final LinearLayout historyLayout = new LinearLayout(Sale.this);
+
                                              historyLayout.setOrientation(LinearLayout.VERTICAL);
                                              historyLayout.addView(historyList);
 
@@ -128,14 +130,12 @@ public class Sale extends MainActivity {
                                                 historyDisplay.add(beerName.get(beerHistoryName.get(position).get(i)) + " x " + beerHistoryClick.get(position).get(i));
                                              }
                                              alertHistory.setMessage(itemList.get(position));
-                                             alertHistory.setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
+                                          
+                                             alertHistory.setOnCancelListener(new DialogInterface.OnCancelListener() {
                                                  @Override
-                                                 public void onClick(DialogInterface dialog, int which) {
-                                                     // TODO Auto-generated method stub
-                                                     dialog.dismiss();
+                                                 public void onCancel(DialogInterface dialogInterface) {
                                                     historyLayout.removeView(historyList);
                                                  }
-
                                              });
                                              alertHistory.show();
                                          }
@@ -303,10 +303,13 @@ public class Sale extends MainActivity {
         itemList.clear();
         tabAmount.clear();
 
+        beerHistoryClick.clear();
+        beerHistoryName.clear();
+
         beerItem.clear();
         openTabs.clear();
         if (folder.exists()) {
-        Log.d("getBeerData","Folder exists");
+            Log.d("getBeerData", "Folder exists");
 
             final String filenameBeers = folder.toString() + "/" + "BeerPOS_BEER.csv";
 
@@ -318,15 +321,15 @@ public class Sale extends MainActivity {
                 while ((line = reader.readLine()) != null) {
 
                     String[] RowData = line.split(",");
-                   // Log.d("Beer.csv","RowData: "+RowData[0]+","+RowData[1]+","+RowData[2]+","+RowData[3]);
+                    // Log.d("Beer.csv","RowData: "+RowData[0]+","+RowData[1]+","+RowData[2]+","+RowData[3]);
 
-                    beerName.add(index,RowData[0]);
+                    beerName.add(index, RowData[0]);
                     //beerName.add("test");
-                    beerValue.add(index,Double.parseDouble(RowData[1]));
+                    beerValue.add(index, Double.parseDouble(RowData[1]));
                     //beerValue.add((double) 1);
-                    beerClicks.add(index,Integer.parseInt(RowData[2]));
+                    beerClicks.add(index, Integer.parseInt(RowData[2]));
                     //beerClicks.add(0);
-                    beerOnTap.add(index,Boolean.valueOf(RowData[3]));
+                    beerOnTap.add(index, Boolean.valueOf(RowData[3]));
                     beerItem.add("beer");
 
                     index++;
@@ -353,8 +356,8 @@ public class Sale extends MainActivity {
                     itemList.add(RowData[0]);
                     //itemList.add("Dan");
                     tabAmount.add(Double.parseDouble(RowData[1]));
-                    beerHistoryClick.add(tabAmount.size()-1, new ArrayList<Integer>());
-                    beerHistoryName.add(tabAmount.size()-1, new ArrayList<Integer>());
+                    beerHistoryClick.add(tabAmount.size() - 1, new ArrayList<Integer>());
+                    beerHistoryName.add(tabAmount.size() - 1, new ArrayList<Integer>());
                     //tabAmount.add((double) 0);
                     openTabs.add("tab");
                 }
@@ -369,6 +372,29 @@ public class Sale extends MainActivity {
             }
 
         }
+
+
+
+        final String filenameHist = folder.toString() + "/" + "BeerPOS_HIST.csv";
+        FileInputStream is3 = new FileInputStream(filenameHist);
+        BufferedReader reader3 = new BufferedReader(new InputStreamReader(is3));
+        try {
+            String line;
+            while ((line = reader3.readLine()) != null) {
+                String[] RowData = line.split(",");
+               beerHistoryName.get(Integer.parseInt(RowData[0])).add(Integer.parseInt(RowData[1]));
+               beerHistoryClick.get(Integer.parseInt(RowData[0])).add(Integer.parseInt(RowData[2]));
+            }
+        } catch (IOException ex) {
+            // handle exception
+        } finally {
+            try {
+                is3.close();
+            } catch (IOException e) {
+                // handle exception
+            }
+        }
+
         updateBeerList();
         updateTabList();
     }
@@ -561,6 +587,28 @@ public class Sale extends MainActivity {
         }
         fw.flush();
         fw.close();
+
+        final String filenameHist = folder.toString() + "/" + "BeerPOS_HIST.csv";
+
+        FileWriter fw3 = new FileWriter(filenameHist);
+        fw3.write("");
+        if(beerHistoryClick.size()>0) {
+            for (int i = 0; i < beerHistoryClick.size(); i++) {
+                if (beerHistoryName.get(i).size()>0){
+                    for (int j = 0;j < beerHistoryClick.get(i).size(); j ++){
+                        fw3.append(Integer.toString(i));
+                        fw3.append(',');
+                        fw3.append((beerHistoryName.get(i).get(j)).toString());
+                        fw3.append(',');
+                        fw3.append((beerHistoryClick.get(i).get(j)).toString());
+                        fw3.append('\n');
+                    }
+                }
+
+            }
+        }
+        fw3.flush();
+        fw3.close();
 
     }
     public void sendBeerMenu(View view) {
