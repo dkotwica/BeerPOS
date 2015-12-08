cd7bfea5fe38eab1faff1adfda40bab4545f004e
dkotwica <domk123@gmail.com>
2015-12-07 22:52:55 -0500

    Fixed formatting of long variables.

--- app/src/main/java/domdomdom/beerpos/BeerData.java
+++ app/src/main/java/domdomdom/beerpos/BeerData.java
@@ -236,10 +236,10 @@ public class BeerData extends Stats {
                 long kegLeft = (System.currentTimeMillis()/1000 - beerStart.get(i))/3600;
                 double kegPercent = weightValue.get(i)/kegWeight;
                 beer_name.add("ON TAP");
-                beer_name.add("Duration on tap: " + kegLeft);
-                beer_name.add("Number of pints left: " + (kegPercent)*124);
-                beer_name.add("Estimated duration left: " + ((kegPercent)*kegLeft) + " hours");
-                beer_name.add("Revenue per hour: " + (((1-kegPercent)*124)*beerValue.get(i)));
+                beer_name.add("Duration on tap: " + String.format("%02d", kegLeft) + " hours");
+                beer_name.add("Number of pints left: " + String.format("%02d", (kegPercent)*124) + " pints");
+                beer_name.add("Estimated duration left: " + String.format("%02d", ((kegPercent)*kegLeft)) + " hours");
+                beer_name.add("Revenue per hour: " + "$" + String.format("%02d", (((1-kegPercent)*124)*beerValue.get(i))));
             }
             else {
                 //Doesn't take into account store hours
@@ -247,10 +247,10 @@ public class BeerData extends Stats {
                 beer_name.add("NOT ON TAP");
                 beer_name.add("Date put on tap: " + new Date(beerStart.get(i)*1000));
                 beer_name.add("Date taken off tap: " + new Date(beerEnd.get(i)*1000));
-                beer_name.add("Time taken for beer to empty: " + kegDuration + " hours");
-                beer_name.add("Average pints per hour: " + (124/kegDuration) + " pints");
-                beer_name.add("Revenue generated: " + "$" + (beerValue.get(i)*124));
-                beer_name.add("Revenue per hour: " + "$" + ((beerValue.get(i)*124)/kegDuration));
+                beer_name.add("Time taken for beer to empty: " + String.format("%02d", kegDuration) + " hours");
+                beer_name.add("Average pints per hour: " + String.format("%02d", (124/kegDuration)) + " pints");
+                beer_name.add("Revenue generated: " + "$" + String.format("%02d", (beerValue.get(i)*124)));
+                beer_name.add("Revenue per hour: " + "$" + String.format("%02d", ((beerValue.get(i)*124)/kegDuration)));
             }
             BarDetails.put(beerName.get(i), beer_name);
         }
