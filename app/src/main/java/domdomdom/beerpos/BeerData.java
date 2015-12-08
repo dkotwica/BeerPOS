6299e209abf5ffa0ab14a979d6df9757a9f09e92
dkotwica <domk123@gmail.com>
2015-12-07 22:30:48 -0500

    Stats page finished

--- app/src/main/java/domdomdom/beerpos/BeerData.java
+++ app/src/main/java/domdomdom/beerpos/BeerData.java
@@ -12,6 +12,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -23,7 +24,7 @@ import java.util.TreeMap;
 public class BeerData extends Stats {
     ArrayList<String> beerName = new ArrayList<>();
     ArrayList<Boolean> beerOnTap = new ArrayList<>();
-
+    ArrayList<Double> weightValue = new ArrayList<>();
     ArrayList<Double> beerValue = new ArrayList<>();
     ArrayList<Integer> beerClicks = new ArrayList<>();
     ArrayList<Long> beerStart = new ArrayList<>();
@@ -107,6 +108,65 @@ public class BeerData extends Stats {
         }
         updateBeerList();
     }
+    private void saveWeightData() throws IOException {
+        File folder = new File(Environment.getExternalStorageDirectory()
+                + "/BeerPOS");
+
+        boolean var = false;
+        if (!folder.exists())
+            var = folder.mkdir();
+
+        final String filenameWeight = folder.toString() + "/" + "BeerPOS_WEIGHT.csv";
+
+
+        FileWriter fw = new FileWriter(filenameWeight);
+        fw.write("");
+        if (weightValue.size() > 0) {
+            for (int i = 0; i < weightValue.size(); i++) {
+                fw.append((weightValue.get(i)).toString());
+                fw.append('\n');
+            }
+        }
+        fw.flush();
+        fw.close();
+    }
+
+    private void getWeightData() throws IOException {
+        Log.d("getBeerData", "Sucessfully called the getBeerData()");
+        File folder = new File(Environment.getExternalStorageDirectory()
+                + "/BeerPOS");
+
+        weightValue.clear();
+        if (folder.exists()) {
+            Log.d("getBeerData", "Folder exists");
+
+            final String filenameWeight = folder.toString() + "/" + "BeerPOS_WEIGHT.csv";
+
+            FileInputStream is = new FileInputStream(filenameWeight);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+            try {
+                int index = 0;
+                String line;
+                while ((line = reader.readLine()) != null) {
+
+                    String[] RowData = line.split(",");
+                    // Log.d("Beer.csv","RowData: "+RowData[0]+","+RowData[1]+","+RowData[2]+","+RowData[3]);
+
+                    weightValue.add(index, Double.valueOf(RowData[0]));
+                    index++;
+
+                }
+            } catch (IOException ex) {
+                // handle exception
+            } finally {
+                try {
+                    is.close();
+                } catch (IOException e) {
+                    // handle exception
+                }
+            }
+        }
+    }
     private void updateBeerList(){
         storedBeer.clear();
         for (int i = 0; i < beerName.size(); i++) {
@@ -167,16 +227,27 @@ public class BeerData extends Stats {
 
         for(int i = 0; i < beerName.size(); i++) {
             List<String> beer_name = new ArrayList<String>();
-            beer_name.add(String.valueOf(weightValue));
+
 
             if (beerOnTap.get(i) == true ) {
-
+                long kegLeft = (System.currentTimeMillis()/1000 - beerStart.get(i))/3600;
+                double kegPercent = weightValue.get(i)/kegWeight;
+                beer_name.add("ON TAP");
+                beer_name.add("Duration on tap: " + kegLeft);
+                beer_name.add("Number of pints left: " + (kegPercent)*124);
+                beer_name.add("Estimated duration left: " + ((kegPercent)*kegLeft) + " hours");
+                beer_name.add("Revenue per hour: " + (((1-kegPercent)*124)*beerValue.get(i)));
             }
             else {
                 //Doesn't take into account store hours
-                long kegDuration = ((beerStart.get(i) - beerEnd.get(i))*3600);
+                long kegDuration = ((beerEnd.get(i) - beerStart.get(i))/3600);
+                beer_name.add("NOT ON TAP");
+                beer_name.add("Date put on tap: " + new Date(beerStart.get(i)*1000));
+                beer_name.add("Date taken off tap: " + new Date(beerEnd.get(i)*1000));
                 beer_name.add("Time taken for beer to empty: " + kegDuration + " hours");
-                beer_name.add("Average pints per hour: " + (weightValue/kegWeight) );
+                beer_name.add("Average pints per hour: " + (124/kegDuration));
+                beer_name.add("Revenue generated: " + (beerValue.get(i)*124));
+                beer_name.add("Revenue per hour: " + ((beerValue.get(i)*124)/kegDuration));
             }
             BarDetails.put(beerName.get(i), beer_name);
         }
